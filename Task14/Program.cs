c7d5ecb91030cf6bbc7252af0d7e284facebfe07
Aghavni Zatikyan <asya.zatikyan.24@gmail.com>
2023-02-06 02:30:12 +0300

    Add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу квадратa от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+5 2 -> 1,4.
+*/
+int GetNumber(string massege)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.Write(massege);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+void GetSquare(int n)
+ {
+for (int i = 1; i <= n; i++)
+{
+Console.WriteLine($"{i*i}, ");
+}
+}
+
+int n = GetNumber("Введите число N: ");
+GetSquare (n) ;
